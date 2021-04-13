c99681f7ba0b422959f7e1783ca2e8f6459f630f
Andrei Farc <andrei@magnetize.co.uk>
2021-04-13 09:28:27 +0100

    Add project files.

---
+++ TechnicalTestXBIM/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using TechnicalTestXBIM.Models;
+
+namespace TechnicalTestXBIM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFileProvider _fileProvider;
+
+        public HomeController(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Files()
+        {
+            var model = new FilesViewModel();
+            foreach (var item in _fileProvider.GetDirectoryContents(""))
+            {
+                if (IsIfcType(item.PhysicalPath))
+                {
+                    model.Files.Add(
+                        new FileDetails { Name = item.Name, Path = item.PhysicalPath });
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+                return Content("Files not selected");
+
+            foreach (var file in files)
+            {
+                if (file.Length == 0)
+                    return Content("One or more files is empty. Please upload a valid IFC file.");
+
+                var supportedTypes = new[] { "ifc" };
+                var fileExt = Path.GetExtension(file.FileName).Substring(1);
+
+                if (!supportedTypes.Contains(fileExt))
+                {    
+                    return Content("File Extension Is InValid. One or more files are not IFC files - Please only Upload IFC File");                   
+                }
+            }     
+
+            foreach (var file in files)
+            {
+                var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "wwwroot",
+                        file.GetFilename());
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return RedirectToAction("Files");
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        #region Private Methods
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".ifc", "application/ifc"},                
+            };
+        }
+
+        private bool IsIfcType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+
+            if(types.ContainsKey(ext))
+                return true;
+
+            return false;
+             
+        }
+
+        #endregion
+    }
+}
---
+++ TechnicalTestXBIM/Controllers/WebApiController.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Web.Http;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TechnicalTestXBIM.Controllers
+{
+    public class WebApiController: ApiController
+    {
+
+        // GET webapi/getinstances
+        public string GetInstances()
+        { 
+            using (var model = IfcStore.Open(GetSampleFile()))
+            {
+                var walls = model.Instances.OfType<IIfcWall>().Count();
+                var doors = model.Instances.OfType<IIfcDoor>().Count();
+                var windows = model.Instances.OfType<IIfcWindow>().Count();
+
+                var instances = new { Walls = walls, Doors = doors, Windows = windows };
+
+                string json = JsonConvert.SerializeObject(instances, Formatting.Indented);
+
+                return json;
+            }
+        }
+
+        // GET webapi/getrooms
+        public string GetRooms()
+        {  
+            using (var model = IfcStore.Open(GetSampleFile()))
+            {
+                //Get all spaces in the model. 
+                //We use ToList() here to avoid multiple enumeration with Count() and foreach(){}
+                var spaces = model.Instances.OfType<IIfcSpace>().ToList();
+                var rooms = new List<object>();
+                
+                foreach (var space in spaces)
+                {
+                    //write report data
+                    var room = new { RoomName = space.Name, Area = GetArea((IIfcSpace)space) };
+                    rooms.Add(room);                    
+                }
+
+                var json = JsonConvert.SerializeObject(rooms, Formatting.Indented);
+                return json;
+            }
+        }
+
+        #region Private Methods
+
+        private static IIfcValue GetArea(IIfcProduct product)
+        {
+            //try to get the value from quantities first
+            var area =
+                //get all relations which can define property and quantity sets
+                product.IsDefinedBy
+
+                //Search across all property and quantity sets. 
+                //You might also want to search in a specific quantity set by name
+                .SelectMany(r => r.RelatingPropertyDefinition.PropertySetDefinitions)
+
+                //Only consider quantity sets in this case.
+                .OfType<IIfcElementQuantity>()
+
+                //Get all quantities from all quantity sets
+                .SelectMany(qset => qset.Quantities)
+
+                //We are only interested in areas 
+                .OfType<IIfcQuantityArea>()
+
+                //We will take the first one. There might obviously be more than one area properties
+                //so you might want to check the name. But we will keep it simple for this example.
+                .FirstOrDefault()?
+                .AreaValue;
+
+            if (area != null)
+                return area;
+
+            //try to get the value from properties
+            return GetProperty(product, "Area");
+        }
+
+        private static IIfcValue GetProperty(IIfcProduct product, string name)
+        {
+            return
+                //get all relations which can define property and quantity sets
+                product.IsDefinedBy
+
+                //Search across all property and quantity sets. You might also want to search in a specific property set
+                .SelectMany(r => r.RelatingPropertyDefinition.PropertySetDefinitions)
+
+                //Only consider property sets in this case.
+                .OfType<IIfcPropertySet>()
+
+                //Get all properties from all property sets
+                .SelectMany(pset => pset.HasProperties)
+
+                //lets only consider single value properties. There are also enumerated properties, 
+                //table properties, reference properties, complex properties and other
+                .OfType<IIfcPropertySingleValue>()
+
+                //lets make the name comparison more fuzzy. This might not be the best practise
+                .Where(p =>
+                    string.Equals(p.Name, name, System.StringComparison.OrdinalIgnoreCase) ||
+                    p.Name.ToString().ToLower().Contains(name.ToLower()))
+
+                //only take the first. In reality you should handle this more carefully.
+                .FirstOrDefault()?.NominalValue;
+        }
+
+        private static string GetSampleFile()
+        {
+            var sampleFile = Path.Combine(
+                               Directory.GetCurrentDirectory(),
+                               "wwwroot", "SampleHouse4.ifc");
+
+            return sampleFile;
+        }
+
+        #endregion
+    }
+}
